ce229cf54211c21de749d45ed56882af47b8588d
Paridhi Mundra <paridhi20392@iiitd.ac.in>
2026-01-14 16:41:56 +0000

    File changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -98,6 +98,7 @@ export default function Home({ user }) {
                 currentUserEmail={user?.email}
                 userLikes={userLikes}
                 onLikeUpdate={handleLikeUpdate}
+                onDelete={() => queryClient.invalidateQueries({ queryKey: ['posts'] })}
               />
             ))
           )}
